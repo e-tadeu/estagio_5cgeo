9495bd2b75649a082822c1b1e77fce2d7a27910f
Edson Tadeu da Silva Pinto <e.tadeu.eb@ime.eb.br>
2023-10-24 10:47:06 -0300

    inclusão de projeto

--- estagio_5CGEO_provider.py
+++ estagio_5CGEO_provider.py
@@ -38,6 +38,7 @@ from .algorithms.proj3_suavizar_linhas import Projeto3Solucao
 from .algorithms.proj4_fechar_linhas import Projeto4Solucao
 from .algorithms.proj5_aparar_linhas import Projeto5Solucao
 from .algorithms.proj6_expandir_linhas import Projeto6Solucao
+from .algorithms.proj6_unificar_linhas_paralelas import Projeto7Solucao
 
 
 class Estagio5CGEOProvider(QgsProcessingProvider):
@@ -65,6 +66,7 @@ class Estagio5CGEOProvider(QgsProcessingProvider):
         self.addAlgorithm(Projeto4Solucao())
         self.addAlgorithm(Projeto5Solucao())
         self.addAlgorithm(Projeto6Solucao())
+        self.addAlgorithm(Projeto7Solucao())
 
     def id(self):
         """
