22ed1b0effef1d8424825dd4080a85c2dafc0a4e
Edson Tadeu da Silva Pinto <128500530+e-tadeu@users.noreply.github.com>
2023-11-04 16:12:15 -0300

    update

--- algorithms/proj5_aparar_linhas.py
+++ algorithms/proj5_aparar_linhas.py
@@ -255,7 +255,7 @@ class Projeto5Solucao(QgsProcessingAlgorithm):
             nova_feature.setGeometry(geometria_mesclada)
             nova_feature.setAttributes(atributos)
             inputLyr.dataProvider().addFeatures([nova_feature])
-            inputLyr.updateExtents()
+            #inputLyr.updateExtents()
         inputLyr.updateExtents()
 
         #REMOÇÃO DE GEOMETRIAS DUPLICADAS
